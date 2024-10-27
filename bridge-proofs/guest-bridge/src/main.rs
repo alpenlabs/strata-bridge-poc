49c5bc21edd0c84bd58902708ffd1663474f446f
abishek <abishek@alpenlabs.io>
2024-10-27 15:59:10 +0545

    feat: guest code compilation with bitvm bitcoin

--- bridge-proofs/guest-bridge/src/main.rs
+++ bridge-proofs/guest-bridge/src/main.rs
@@ -1,7 +1,7 @@
-use strata_proofimpl_bitvm_bridge::process_checkpoint_proof;
+use strata_proofimpl_bitvm_bridge::process_bridge_proof;
 
 fn main() {
-    let input = sp1_zkvm::io::read();
-    let public_params = process_checkpoint_proof(input);
-    sp1_zkvm::io::commit(&public_params);
+    // let input = sp1_zkvm::io::read();
+    // let public_params = process_bridge_proof(input);
+    // sp1_zkvm::io::commit(&public_params);
 }
