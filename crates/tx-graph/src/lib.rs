1b1a2c83b61a3d0e5ae28860d8018d3a25c6c726
Rajil Bajracharya <imrb1213@gmail.com>
2024-10-28 15:30:59 +0545

    chore!: nuke all bitvm repo code

--- crates/tx-graph/src/lib.rs
+++
@@ -1,9 +0,0 @@
-//! Defines traits and types related to bridge-specific transactions.
-pub mod connectors;
-pub mod constants;
-pub mod graphs;
-pub mod scripts;
-pub mod serialization;
-pub mod transactions;
-pub mod treepp;
-pub mod utils;
