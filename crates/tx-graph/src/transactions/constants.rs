02561e0a1011c9b6281e493941e528bdbd77bc06
Rajil Bajracharya <imrb1213@gmail.com>
2024-10-30 17:43:01 +0545

    fix: apply lint fixes

--- crates/tx-graph/src/transactions/constants.rs
+++ crates/tx-graph/src/transactions/constants.rs
@@ -10,7 +10,7 @@ pub const NUM_ASSERT_DATA_TX2_A256_PK7: usize = 2;
 pub const NUM_ASSERT_DATA_TX2_A160_PK4: usize = 1;
 
 pub const TOTAL_CONNECTORS: usize = NUM_CONNECTOR_A256 + NUM_CONNECTOR_A160 + 1; // +1 for the
-                                                                                 // residual A160 connecor with 4 scripts
+                                                                                 // residual A160 connector with 4 scripts
 pub const TOTAL_VALUES: usize = NUM_ASSERT_DATA_TX1
     * (NUM_ASSERT_DATA_TX1_A160_PK11 + NUM_ASSERT_DATA_TX1_A256_PK7)
     + NUM_ASSERT_DATA_TX2
