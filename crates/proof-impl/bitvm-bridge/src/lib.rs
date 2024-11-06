529e01c4cca484a835adc4fe43eb760358783afa
abishek <abishek@alpenlabs.io>
2024-11-06 15:33:51 +0545

    feat added treepp

--- crates/proof-impl/bitvm-bridge/src/lib.rs
+++ crates/proof-impl/bitvm-bridge/src/lib.rs
@@ -1,5 +1,16 @@
 mod primitives;
 mod process;
+mod treepp;
 
 pub use primitives::{BridgeProofInput, BridgeProofPublicParams};
 pub use process::process_bridge_proof;
+
+#[cfg(test)]
+mod test {
+    use crate::treepp;
+
+    #[test]
+    fn test_once() {
+        // let a = treepp::ExecuteInfo;
+    }
+}
